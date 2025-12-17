6512064c2b8bc207b7fc677538d8730257d6fdfb
stevenfarless <stevenfarless@users.noreply.github.com>
2025-12-17 12:36:05 +0000

    deploy: 06130762bfab6154224f7a1e0bea3c07cc8b4102

--- module/js/modules/bible-structure.js
+++ module/js/modules/bible-structure.js
@@ -1,62 +1,170 @@
-// js/modules/bible-structure.js NEW VERSION
-
-export const BOOK_ABBREVIATIONS = {
-    Genesis: 'Gen', Exodus: 'Exod', Leviticus: 'Lev', Numbers: 'Num', Deuteronomy: 'Deut',
-    Joshua: 'Josh', Judges: 'Judg', Ruth: 'Ruth', '1 Samuel': '1Sam', '2 Samuel': '2Sam',
-    '1 Kings': '1Kgs', '2 Kings': '2Kgs', '1 Chronicles': '1Chr', '2 Chronicles': '2Chr',
-    Ezra: 'Ezra', Nehemiah: 'Neh', Esther: 'Esth', Job: 'Job', Psalms: 'Ps',
-    Proverbs: 'Prov', Ecclesiastes: 'Eccl', 'Song of Solomon': 'Song', Isaiah: 'Isa',
-    Jeremiah: 'Jer', Lamentations: 'Lam', Ezekiel: 'Ezek', Daniel: 'Dan', Hosea: 'Hos',
-    Joel: 'Joel', Amos: 'Amos', Obadiah: 'Obad', Jonah: 'Jonah', Micah: 'Mic',
-    Nahum: 'Nah', Habakkuk: 'Hab', Zephaniah: 'Zeph', Haggai: 'Hag', Zechariah: 'Zech',
-    Malachi: 'Mal', Matthew: 'Matt', Mark: 'Mark', Luke: 'Luke', John: 'John',
-    Acts: 'Acts', Romans: 'Rom', '1 Corinthians': '1Cor', '2 Corinthians': '2Cor',
-    Galatians: 'Gal', Ephesians: 'Eph', Philippians: 'Phil', Colossians: 'Col',
-    '1 Thessalonians': '1Thes', '2 Thessalonians': '2Thes', '1 Timothy': '1Tim',
-    '2 Timothy': '2Tim', Titus: 'Titus', Philemon: 'Phlm', Hebrews: 'Heb', James: 'Jas',
-    '1 Peter': '1Pet', '2 Peter': '2Pet', '1 John': '1John', '2 John': '2John',
-    '3 John': '3John', Jude: 'Jude', Revelation: 'Rev',
-};
+// js/modules/bible-structure.js
+// Responsibility: canonical book order, chapter counts, abbreviations, testament
 
 export const BIBLE_STRUCTURE = {
-    'Old Testament': {
-        Genesis: 50, Exodus: 40, Leviticus: 27, Numbers: 36, Deuteronomy: 34,
-        Joshua: 24, Judges: 21, Ruth: 4, '1 Samuel': 31, '2 Samuel': 24,
-        '1 Kings': 22, '2 Kings': 25, '1 Chronicles': 29, '2 Chronicles': 36,
-        Ezra: 10, Nehemiah: 13, Esther: 10, Job: 42, Psalms: 150, Proverbs: 31,
-        Ecclesiastes: 12, 'Song of Solomon': 8, Isaiah: 66, Jeremiah: 52,
-        Lamentations: 5, Ezekiel: 48, Daniel: 12, Hosea: 14, Joel: 3, Amos: 9,
-        Obadiah: 1, Jonah: 4, Micah: 7, Nahum: 3, Habakkuk: 3, Zephaniah: 3,
-        Haggai: 2, Zechariah: 14, Malachi: 4,
-    },
-    'New Testament': {
-        Matthew: 28, Mark: 16, Luke: 24, John: 21, Acts: 28, Romans: 16,
-        '1 Corinthians': 16, '2 Corinthians': 13, Galatians: 6, Ephesians: 6,
-        Philippians: 4, Colossians: 4, '1 Thessalonians': 5, '2 Thessalonians': 3,
-        '1 Timothy': 6, '2 Timothy': 4, Titus: 3, Philemon: 1, Hebrews: 13,
-        James: 5, '1 Peter': 5, '2 Peter': 3, '1 John': 5, '2 John': 1,
-        '3 John': 1, Jude: 1, Revelation: 22,
-    },
+  'Old Testament': {
+    Genesis: 50,
+    Exodus: 40,
+    Leviticus: 27,
+    Numbers: 36,
+    Deuteronomy: 34,
+    Joshua: 24,
+    Judges: 21,
+    Ruth: 4,
+    '1 Samuel': 31,
+    '2 Samuel': 24,
+    '1 Kings': 22,
+    '2 Kings': 25,
+    '1 Chronicles': 29,
+    '2 Chronicles': 36,
+    Ezra: 10,
+    Nehemiah: 13,
+    Esther: 10,
+    Job: 42,
+    Psalms: 150,
+    Proverbs: 31,
+    Ecclesiastes: 12,
+    'Song of Solomon': 8,
+    Isaiah: 66,
+    Jeremiah: 52,
+    Lamentations: 5,
+    Ezekiel: 48,
+    Daniel: 12,
+    Hosea: 14,
+    Joel: 3,
+    Amos: 9,
+    Obadiah: 1,
+    Jonah: 4,
+    Micah: 7,
+    Nahum: 3,
+    Habakkuk: 3,
+    Zephaniah: 3,
+    Haggai: 2,
+    Zechariah: 14,
+    Malachi: 4,
+  },
+  'New Testament': {
+    Matthew: 28,
+    Mark: 16,
+    Luke: 24,
+    John: 21,
+    Acts: 28,
+    Romans: 16,
+    '1 Corinthians': 16,
+    '2 Corinthians': 13,
+    Galatians: 6,
+    Ephesians: 6,
+    Philippians: 4,
+    Colossians: 4,
+    '1 Thessalonians': 5,
+    '2 Thessalonians': 3,
+    '1 Timothy': 6,
+    '2 Timothy': 4,
+    Titus: 3,
+    Philemon: 1,
+    Hebrews: 13,
+    James: 5,
+    '1 Peter': 5,
+    '2 Peter': 3,
+    '1 John': 5,
+    '2 John': 1,
+    '3 John': 1,
+    Jude: 1,
+    Revelation: 22,
+  },
+};
+
+export const BOOK_ABBREVIATIONS = {
+  Genesis: 'Gen',
+  Exodus: 'Exod',
+  Leviticus: 'Lev',
+  Numbers: 'Num',
+  Deuteronomy: 'Deut',
+  Joshua: 'Josh',
+  Judges: 'Judg',
+  Ruth: 'Ruth',
+  '1 Samuel': '1Sam',
+  '2 Samuel': '2Sam',
+  '1 Kings': '1Kgs',
+  '2 Kings': '2Kgs',
+  '1 Chronicles': '1Chr',
+  '2 Chronicles': '2Chr',
+  Ezra: 'Ezra',
+  Nehemiah: 'Neh',
+  Esther: 'Esth',
+  Job: 'Job',
+  Psalms: 'Ps',
+  Proverbs: 'Prov',
+  Ecclesiastes: 'Eccl',
+  'Song of Solomon': 'Song',
+  Isaiah: 'Isa',
+  Jeremiah: 'Jer',
+  Lamentations: 'Lam',
+  Ezekiel: 'Ezek',
+  Daniel: 'Dan',
+  Hosea: 'Hos',
+  Joel: 'Joel',
+  Amos: 'Amos',
+  Obadiah: 'Obad',
+  Jonah: 'Jonah',
+  Micah: 'Mic',
+  Nahum: 'Nah',
+  Habakkuk: 'Hab',
+  Zephaniah: 'Zeph',
+  Haggai: 'Hag',
+  Zechariah: 'Zech',
+  Malachi: 'Mal',
+  Matthew: 'Matt',
+  Mark: 'Mark',
+  Luke: 'Luke',
+  John: 'John',
+  Acts: 'Acts',
+  Romans: 'Rom',
+  '1 Corinthians': '1Cor',
+  '2 Corinthians': '2Cor',
+  Galatians: 'Gal',
+  Ephesians: 'Eph',
+  Philippians: 'Phil',
+  Colossians: 'Col',
+  '1 Thessalonians': '1Thes',
+  '2 Thessalonians': '2Thes',
+  '1 Timothy': '1Tim',
+  '2 Timothy': '2Tim',
+  Titus: 'Titus',
+  Philemon: 'Phlm',
+  Hebrews: 'Heb',
+  James: 'Jas',
+  '1 Peter': '1Pet',
+  '2 Peter': '2Pet',
+  '1 John': '1John',
+  '2 John': '2John',
+  '3 John': '3John',
+  Jude: 'Jude',
+  Revelation: 'Rev',
 };
 
 export function getAllBooks() {
-    return [
-        ...Object.keys(BIBLE_STRUCTURE['Old Testament']),
-        ...Object.keys(BIBLE_STRUCTURE['New Testament']),
-    ];
+  return [
+    ...Object.keys(BIBLE_STRUCTURE['Old Testament']),
+    ...Object.keys(BIBLE_STRUCTURE['New Testament']),
+  ];
 }
 
 export function getChapterCount(book) {
-    for (const testament in BIBLE_STRUCTURE) {
-        if (BIBLE_STRUCTURE[testament][book]) {
-            return BIBLE_STRUCTURE[testament][book];
-        }
+  for (const testament in BIBLE_STRUCTURE) {
+    if (Object.prototype.hasOwnProperty.call(BIBLE_STRUCTURE[testament], book)) {
+      return BIBLE_STRUCTURE[testament][book];
     }
-    return 0;
+  }
+  return 0;
 }
 
 export function getTestament(book) {
-    if (BIBLE_STRUCTURE['Old Testament'][book]) return 'Old Testament';
-    if (BIBLE_STRUCTURE['New Testament'][book]) return 'New Testament';
-    return null;
+  if (Object.prototype.hasOwnProperty.call(BIBLE_STRUCTURE['Old Testament'], book)) {
+    return 'Old Testament';
+  }
+  if (Object.prototype.hasOwnProperty.call(BIBLE_STRUCTURE['New Testament'], book)) {
+    return 'New Testament';
+  }
+  return null;
 }
