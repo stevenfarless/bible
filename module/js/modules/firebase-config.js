29fa9695aa35e08a107c16ff5a87f7f5c6667aab
stevenfarless <stevenfarless@users.noreply.github.com>
2025-12-19 12:13:38 +0000

    deploy: 3b49a261fa379ec0fc787fc500e0efe317aeb795

--- module/js/modules/firebase-config.js
+++ module/js/modules/firebase-config.js
@@ -25,12 +25,13 @@ import { getDatabase } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-
  */
 
 const firebaseConfig = {
-  projectId: "esv-bible-6dffb",
+  apiKey: "AIzaSyCGVPqbTZCQ3Hrs9sFIJm_PR32FP_CVXSw",
   authDomain: "esv-bible-6dffb.firebaseapp.com",
   databaseURL: "https://esv-bible-6dffb-default-rtdb.firebaseio.com",
+  projectId: "esv-bible-6dffb",
   storageBucket: "esv-bible-6dffb.firebasestorage.app",
   messagingSenderId: "824462651620",
-  appId: "1:824462651620:web:5f46fe033ac46d2329bcf1"
+  appId: "1:824462651620:web:5f46fe033ac46d2329bcf1",
 };
 
 /**
