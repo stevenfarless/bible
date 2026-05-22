2f566a7a4b69dfe60378e63ec67df6c770fb6bcd
stevenfarless <stevenfarless@users.noreply.github.com>
2026-05-22 04:50:02 +0000

    deploy: 583a5515404ee012a0645f41629e66f2d51765b5

--- json-ver/sw.js
+++ json-ver/sw.js
@@ -1,4 +1,4 @@
-const BUILD_ID = "903a3e565b317bc78a74a146d8fc9e5333ca6a2a";
+const BUILD_ID = "583a5515404ee012a0645f41629e66f2d51765b5";
 const CACHE_NAME = `esv-bible-${BUILD_ID}`;
 
 self.addEventListener('install', () => {
