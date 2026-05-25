6a0a2e00d094fbfdcd622bdce7b3a2798a09449d
stevenfarless <stevenfarless@users.noreply.github.com>
2026-05-25 14:53:04 +0000

    deploy: 9f8387c8c7e7e47d0b373500861d2c960e8d3494

--- dev/sw.js
+++ dev/sw.js
@@ -1,4 +1,4 @@
-const BUILD_ID = "c8208c52438ae2afd2d6dd0771552d9d1c6b32ea";
+const BUILD_ID = "9f8387c8c7e7e47d0b373500861d2c960e8d3494";
 const CACHE_NAME = `esv-bible-${BUILD_ID}`;
 
 // App shell JS modules — always fetched from the network so a refresh
