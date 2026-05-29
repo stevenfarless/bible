5b5db5378e697b93a47cd2ab9efdceef2f1c4398
stevenfarless <stevenfarless@users.noreply.github.com>
2026-05-29 04:55:46 +0000

    deploy: caa9553c760392ac9941b6d93be49507805348c2

--- feat-per-book-translation-files/sw.js
+++ feat-per-book-translation-files/sw.js
@@ -1,4 +1,4 @@
-const BUILD_ID = "1815d26f3f313cdbb5c76bfa179058f02ff4e990";
+const BUILD_ID = "caa9553c760392ac9941b6d93be49507805348c2";
 const CACHE_NAME = `bible-${BUILD_ID}`;
 
 // App shell assets (JS modules + CSS): network-first, bypass the browser
