87277e7c03bb52e8d3239945e9d80a4988ca995a
stevenfarless <stevenfarless@users.noreply.github.com>
2026-06-01 07:50:24 +0000

    deploy: f15d1fd041af538da87a8fdad23636a2051090d0

--- exp/sw.js
+++ exp/sw.js
@@ -1,4 +1,4 @@
-const BUILD_ID = "a15bf2402c5faab703a253de9ad075179305c57c";
+const BUILD_ID = "f15d1fd041af538da87a8fdad23636a2051090d0";
 const CACHE_NAME = `bible-${BUILD_ID}`;
 
 // App shell assets (JS modules + CSS): network-first, bypass the browser
