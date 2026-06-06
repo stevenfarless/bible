dbf2ed99edd0c94fb2e8019eeff30a86f8712b48
Steven <125781365+stevenfarless@users.noreply.github.com>
2026-06-06 04:28:57 -0500

    Increase fuzzy search threshold to 1

--- bible-api.js
+++ bible-api.js
@@ -312,7 +312,7 @@ export class BibleApi {
                     idField: 'id',
                     searchOptions: {
                         prefix: true,
-                        fuzzy: 0.15,
+                        fuzzy: 1,  // PREVIOUS VALUE fuzzy: 0.15,
                         combineWith: 'AND',
                     },
                 });
